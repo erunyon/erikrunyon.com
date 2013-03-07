37201a83ac5db217a3b0b72c8d1b90fb35e4b76d
Erik Runyon <erunyon@gmail.com>
2013-03-06 20:04:56 -0500

    clean up sass paths to get images in the right places

--- config.rb
+++ config.rb
@@ -3,14 +3,14 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_images_path = "/css/images"
+# http_fonts_path = "/fonts"
+css_dir = "public/css"
 
 # Local development paths
 sass_dir = "sass"
-images_dir = "source/stylesheets/images"
-fonts_dir = "source/fonts"
+images_dir = "source/css/images"
+# fonts_dir = "source/fonts"
 
 line_comments = false
 output_style = :compressed
