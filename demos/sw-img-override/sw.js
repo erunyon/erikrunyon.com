2a79254726cbaeef6f7fbba85e50b2259d284863
Erik Runyon <erunyon@gmail.com>
2016-04-09 15:14:23 -0400

    Add SW image override demo

---
+++ demos/sw-img-override/sw.js
@@ -0,0 +1,36 @@
+'use strict';
+
+self.addEventListener('install', event => {
+  console.log('installing…');
+  // Uncomment the line below to activate the SW faster
+  //self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('activating…');
+  // Uncomment the line below if you want the SW to claim immediately
+  //self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  let request = event.request;
+
+  // Return the request if HTML
+  if(request.headers.get('Accept').indexOf('text/html') !== -1){
+    event.respondWith( fetch(request) );
+    return;
+  }
+
+  // Intercept and return a random image if request is for an "image"
+  if(request.headers.get('Accept').indexOf('image') !== -1){
+    let rand = (Math.floor(Math.random() * 200) + 200);
+
+    event.respondWith(
+      fetch(
+        'https://www.placecage.com/' + rand + '/300',
+        {mode: 'no-cors'}
+      )
+    );
+  }
+
+});
