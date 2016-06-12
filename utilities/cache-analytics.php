6bc9bf5ac5157f9ba6a21c520bd9e1d52f541c59
Erik Runyon <erunyon@gmail.com>
2016-06-11 22:16:28 -0400

    update to use server cached analytics scripts

--- utilities/cache-analytics.php
+++ utilities/cache-analytics.php
@@ -1,6 +1,11 @@
 <?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
-$utilities_dir = '/var/www/html/er/utilities/';
+// 0 * * * *  cd /var/www/html/er/erikrunyon.com/utilities; /usr/bin/php5 cache-analytics.php
+
+$utilities_dir = '/var/www/html/er/erikrunyon.com/utilities/';
 $ga = file_get_contents('https://www.google-analytics.com/analytics.js');
 $gauges = file_get_contents('https://secure.gaug.es/track.js');
 
